3b747fdf51a0ae5eb1f29a0ccdfa36187487d65c
lsw <schu132032@students.bwv-ahaus.de>
2022-09-07 10:40:30 +0200

    add sort to User

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import {
     createTheme,
     Table,
     TableBody,
-    TableCell,
+    TableCell, TableHead,
     TableRow,
     TextField,
     ThemeProvider
@@ -22,18 +22,29 @@ const darkTheme = createTheme({
 
 function App() {
 
-    const [uncategorizedItems, setuncategorizedItems] = useState<string[]>([]);
+    const [uncategorizedItems, setUncategorizedItems] = useState<string[]>([]);
     const [text, setText] = useState<string>("");
+    const [categorizedItems, setCategorizedItems] = useState<string[]>([]);
 
-    function handleDelete(indexToDelete: number) {
-        setuncategorizedItems(uncategorizedItems => uncategorizedItems.filter((value, index: number) => index !== indexToDelete))
+    function handleDeleteUncategorized(indexToDelete: number) {
+        setUncategorizedItems(uncategorizedItems => uncategorizedItems.filter((value, index: number) => index !== indexToDelete))
+    }
+
+    function handleDeleteCategorized(indexToDelete: number) {
+        setCategorizedItems(uncategorizedItems => uncategorizedItems.filter((value, index: number) => index !== indexToDelete))
     }
 
     function handleAdd(item: string) {
         const newList = uncategorizedItems.concat(item);
 
-        setuncategorizedItems(newList);
+        setUncategorizedItems(newList);
         setText("");
+    }
+
+    function addItemsToUser(item: string, index: number) {
+        const newList = categorizedItems.concat(item);
+        setCategorizedItems(newList)
+        handleDeleteUncategorized(index)
     }
 
     return (
@@ -42,28 +53,39 @@ function App() {
             <div id="header"></div>
             <div id="main">
                 <Table id={"userAndItems"}>
+                    <TableHead>
+                        <TableRow>
+
+                        </TableRow>
+                    </TableHead>
                     <TableBody>
                         <TableRow>
-                            <TableCell>User</TableCell>
-                            <TableCell></TableCell>
-                            <TableCell></TableCell>
-                            <TableCell></TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell></TableCell>
-                            <TableCell></TableCell>
+                            <TableCell>
+                                {
+                                    categorizedItems.map((categorizedItem, index) =>
+                                        <Chip
+                                            key={categorizedItem}
+                                            label={categorizedItem}
+                                            onDelete={() => handleDeleteCategorized(index)}
+                                            sx={{mt: 1}}
+                                        ></Chip>
+                                    )
+                                }
+                            </TableCell>
                         </TableRow>
                     </TableBody>
                 </Table>
                 <div id={"uncategorizedItemList"}>
-                    {uncategorizedItems.map((uncategorizedItem, index) =>
-                        <Chip
-                            key={uncategorizedItem}
-                            label={uncategorizedItem}
-                            onDelete={() => handleDelete(index)}
-                            sx={{mr: 1, mb: 1}}
-                        ></Chip>
-                    )}
+                    {
+                        uncategorizedItems.map((uncategorizedItem, index) =>
+                            <Chip
+                                key={uncategorizedItem}
+                                label={uncategorizedItem}
+                                onDelete={() => handleDeleteUncategorized(index)}
+                                sx={{mr: 1, mb: 1}}
+                                onClick={() => addItemsToUser(uncategorizedItem, index)}
+                            ></Chip>
+                        )}
                 </div>
                 <TextField value={text} variant={"outlined"} className={"addItem"} id={"itemName"}
                            onChange={event => setText(event.target.value)}></TextField>
