7df27a253383f81481815f2ee96a32c3abff7484
lsw <schu132032@students.bwv-ahaus.de>
2022-09-08 10:24:20 +0200

    add entry inputa

--- frontend/src/_shared/components/EventPage.tsx
+++ frontend/src/_shared/components/EventPage.tsx
@@ -1,34 +1,34 @@
 import React, {useState} from 'react';
 import './CSS/EventPage.css';
 import {
-  Button, Chip,
-  createTheme, Grid, Stack,
-  Table,
-  TableBody,
-  TableCell, TableContainer, TableHead,
-  TableRow,
-  TextField,
-  ThemeProvider
+    Button, Chip,
+    createTheme, Stack,
+    Table,
+    TableBody,
+    TableCell, TableContainer, TableHead,
+    TableRow,
+    TextField,
+    ThemeProvider
 } from "@mui/material";
 import {connect, ConnectedProps} from "react-redux";
 import {RootState} from "../helpers/store";
 
 const mapState = (state: RootState) => ({
-  themeStoreDarkMode: state.themeStore.darkMode
+    themeStoreDarkMode: state.themeStore.darkMode
 });
 
 const connector = connect(mapState);
 
 const themeDark = createTheme({
-  palette: {
-    mode: "dark"
-  }
+    palette: {
+        mode: "dark"
+    }
 })
 
 const themeLight = createTheme({
-  palette: {
-    mode: "light"
-  }
+    palette: {
+        mode: "light"
+    }
 })
 
 interface Props extends ConnectedProps<typeof connector> {
@@ -36,102 +36,105 @@ interface Props extends ConnectedProps<typeof connector> {
 }
 
 interface item {
-  item: string;
-  user: string;
+    item: string;
+    user: string;
 }
 
 function EventPage(props: Props) {
 
-  const [uncategorizedItems, setUncategorizedItems] = useState<string[]>([]);
-  const [text, setText] = useState<string>("");
-  const [categorizedItems, setCategorizedItems] = useState<item[]>([]);
-  const [users] = useState(['User 1', 'User 2', 'User 3', 'User 4']);
-  const testUser = 'User 3'
+    const [uncategorizedItems, setUncategorizedItems] = useState<string[]>([]);
+    const [text, setText] = useState<string>("");
+    const [categorizedItems, setCategorizedItems] = useState<item[]>([]);
+    const [users] = useState(['User 1', 'User 2', 'User 3', 'User 4', 'User 5', 'User 6', 'User 7', 'User 8', 'User 9', 'User 10', 'User 11', 'User 12']);
+    const testUser = 'User 5'
 
-  function handleDeleteUncategorized(indexToDelete: number) {
-    setUncategorizedItems(uncategorizedItems => uncategorizedItems.filter((value, index: number) => index !== indexToDelete));
-  }
+    function handleDeleteUncategorized(indexToDelete: number) {
+        setUncategorizedItems(uncategorizedItems => uncategorizedItems.filter((value, index: number) => index !== indexToDelete));
+    }
 
-  function handleDeleteCategorized(indexToDelete: number) {
-    setCategorizedItems(uncategorizedItems => uncategorizedItems.filter((value, index: number) => index !== indexToDelete));
-  }
+    function handleDeleteCategorized(indexToDelete: number) {
+        setCategorizedItems(uncategorizedItems => uncategorizedItems.filter((value, index: number) => index !== indexToDelete));
+    }
 
-  function handleAdd(item: string) {
-    const newList = uncategorizedItems.concat(item);
+    function handleAdd(item: string) {
 
-    setUncategorizedItems(newList);
-    setText("");
-  }
+        if(item !== ''){
+        const newList = uncategorizedItems.concat(item);
+        setUncategorizedItems(newList);
+        setText("");
+        }
+    }
 
-  function addItemsToUser(item: string, index: number) {
-    const newList = categorizedItems.concat({item: item, user: testUser});
-    setCategorizedItems(newList);
-    handleDeleteUncategorized(index);
-    console.log(newList);
-  }
+    function addItemsToUser(item: string, index: number) {
+        const newList = categorizedItems.concat({item: item, user: testUser});
+        setCategorizedItems(newList);
+        handleDeleteUncategorized(index);
+        console.log(newList);
+    }
 
-  return (
-      <ThemeProvider theme={props.themeStoreDarkMode ? themeDark : themeLight}>
-        <div className="EventPage">
-          <div id="main">
-            <TableContainer style={{maxHeight: 450}}>
-              <Table id={"userAndItems"} stickyHeader={true}>
-                <TableHead>
-                  <TableRow>
-                    {users.map((user) =>
-                        <TableCell>{user}</TableCell>
-                    )}
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  <TableRow id={'contentRow'}>
-                    {users.map((user) =>
-                        <TableCell>
-                          {
-                            categorizedItems.map((categorizedItem, index) =>
-                                    user === categorizedItem.user && (
-                                        <Stack direction={"column"}>
-                                          <Chip
-                                              key={index}
-                                              label={categorizedItem.item}
-                                              onDelete={() => handleDeleteCategorized(index)}
-                                              sx={{mt: 1}}
-                                              style={{maxWidth: 'max-content'}}
-                                          />
-                                        </Stack>
-                                    )
-                            )
-                          }
-                        </TableCell>
-                    )}
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </TableContainer>
-            <Stack alignItems={"flex-end"} justifyContent={"center"} sx={{height: '30vh'}}>
-              <div id={"uncategorizedItemList"}>
-                {
-                  uncategorizedItems.map((uncategorizedItem, index) =>
-                      <Chip
-                          key={index}
-                          label={uncategorizedItem}
-                          onDelete={() => handleDeleteUncategorized(index)}
-                          sx={{mr: 1, mb: 1}}
-                          onClick={() => addItemsToUser(uncategorizedItem, index)}
-                      />
-                  )}
-              </div>
-            </Stack>
-            <Stack alignItems={"flex-end"} justifyContent={"center"} direction={"row"} sx={{height: '4vh'}}>
-              <TextField value={text} variant={"outlined"} className={"addItem"} id={"itemName"}
-                         onChange={event => setText(event.target.value)}></TextField>
-              <Button className={"addItem"} variant={"contained"} onClick={() => handleAdd(text)}>add</Button>
-            </Stack>
-          </div>
-          <div id="footer"></div>
-        </div>
-      </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={props.themeStoreDarkMode ? themeDark : themeLight}>
+            <div className="EventPage">
+                <div id="main">
+                    <TableContainer style={{maxHeight: 450}}>
+                        <Table id={"userAndItems"} stickyHeader={true}>
+                            <TableHead>
+                                <TableRow>
+                                    {users.map((user) =>
+                                        <TableCell>{user}</TableCell>
+                                    )}
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                <TableRow id={'contentRow'}>
+                                    {users.map((user) =>
+                                        <TableCell style={{verticalAlign: "top"}}>
+                                            {
+                                                categorizedItems.map((categorizedItem, index) =>
+                                                        user === categorizedItem.user && (
+                                                            <Stack direction={"column"}>
+                                                                <Chip
+                                                                    key={index}
+                                                                    label={categorizedItem.item}
+                                                                    onDelete={() => handleDeleteCategorized(index)}
+                                                                    sx={{mt: 1}}
+                                                                    style={{maxWidth: 'max-content'}}
+                                                                />
+                                                            </Stack>
+                                                        )
+                                                )
+                                            }
+                                        </TableCell>
+                                    )}
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <Stack alignItems={"flex-end"} justifyContent={"center"} sx={{height: '30vh'}}>
+                        <div id={"uncategorizedItemList"}>
+                            {
+                                uncategorizedItems.map((uncategorizedItem, index) =>
+                                    <Chip
+                                        key={index}
+                                        label={uncategorizedItem}
+                                        onDelete={() => handleDeleteUncategorized(index)}
+                                        sx={{mr: 1, mb: 1}}
+                                        onClick={() => addItemsToUser(uncategorizedItem, index)}
+                                    />
+                                )}
+                        </div>
+                    </Stack>
+                    <Stack alignItems={"flex-end"} justifyContent={"center"} direction={"row"} sx={{height: '4vh'}}
+                           onKeyPress={(e) => e.key === 'Enter' && handleAdd(text)}>
+                        <TextField value={text} variant={"outlined"} className={"addItem"} id={"itemName"}
+                                   onChange={event => setText(event.target.value)}></TextField>
+                        <Button className={"addItem"} variant={"contained"} onClick={() => handleAdd(text)}>add</Button>
+                    </Stack>
+                </div>
+                <div id="footer"></div>
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default connector(EventPage);
